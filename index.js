4c51acc986f0dd3bee47644e0648138b3163aaf0
Abhik91 <deykumar.abhik@gmail.com>
2020-04-18 03:18:47 -0700

    Added comments & history to index.js

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+
+/**************************************************************************************************
+* Title       - Cloudflare - internship-application-fullstack                                     * 
+* File        - index.js                                                                          * 
+* Description - The code performs the below matrix operations -                                   *
+*               a. Request the URLs from the API                                                  *
+*               b. Request a (random: see #3) variant                                             *
+*               c. Distribute requests between variants (A/B testing style)                       *
+*               d. Changing copy/URLs                                                             *
+*               e. Persisting variants                                                            *
+* Reference   - https://github.com/cloudflare-internship-2020/internship-application-fullstack    *
+* Author      - Abhik Kumar Dey (akd)                                                             *
+* Editor      - Sublime Text / GEdit                                                              *
+* Published   - https://akdey-cloudflare-internship.akdey.workers.dev                             *
+*                                                                                                 *
+*                                History                                                          *
+* Version               Date                   Author                 Description                 *
+***************************************************************************************************     
+* Initial Version       17 April, 2020        Abhik Dey       Initial changes as per requirement  *
+*                                                                                                 *
+*                                                                                                 *
+**************************************************************************************************/
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
@@ -7,6 +30,7 @@ addEventListener('fetch', event => {
  */
 
 
+//URL to make fetch request and parse as JSON
 const url = 'https://cfw-takehome.developers.workers.dev/api/variants';
 
 async function handleRequest(request) {
@@ -15,43 +39,48 @@ async function handleRequest(request) {
     headers: { 'content-type': 'text/plain' },
   })
 */
-	let resp1 = null  	
-  	let variant = -1
+	let resp1 = null;  	
+  let variant = -1;
 
-	let resp = await fetch(url);
-	const data = await resp.json();
+	
+  let resp = await fetch(url);  //Fetching mail URL @akd
+	const data = await resp.json(); // Parsing to JSON @akd
 
-	const URL_1 = data.variants[0]
-  	const URL_2 = data.variants[1]
+  // Defining constant variables for 2 variants of URL - to be used to check cookies @akd
+	const URL_1 = data.variants[0];
+  const URL_2 = data.variants[1];
 	  	
-  	const cookie_url = request.headers.get('cookie');
-  	url_cookie = (cookie_url) ? cookie_url.split('=>')[1] : null;
+  //Check for cookies 
+  const cookie_url = request.headers.get('cookie');
+  url_cookie = (cookie_url) ? cookie_url.split('=>')[1] : null; //Spliting cookies to get one of the variant URLs @akd
   	
 
-  	 if (cookie_url && (url_cookie == URL_1 || url_cookie == URL_2)){
-  	 	resp1 = await fetch(url_cookie);
-  	 	variant = data.variants.indexOf(url_cookie);
-  	 }
-  	 else{
-  	 	//Fetch URL in A/B Testing style (50%)
-  	 	let ab_split_url = (Math.random() > 0.5 ? data.variants[0]: data.variants[1]);
-  		resp1 = await fetch(ab_split_url);
-  		resp1 = new Response(resp1.body,resp1);
-  		resp1.headers.set('Set-Cookie',`cookie-url=>${ab_split_url}`);
-  		variant = data.variants.indexOf(ab_split_url);
-  		}
-		
-		
-  //	}
-  	 	
+  //Persisting variants
+  if (cookie_url && (url_cookie == URL_1 || url_cookie == URL_2)){
+   	resp1 = await fetch(url_cookie);
+   	variant = data.variants.indexOf(url_cookie);
+  }
+  //When user hits URL for the first time, variant selected in A/B style @akd
+  else{
+   	//Fetch URL in A/B Testing style (50%)
+   	let ab_split_url = (Math.random() > 0.5 ? data.variants[0]: data.variants[1]);
+  	resp1 = await fetch(ab_split_url);
+  	resp1 = new Response(resp1.body,resp1);
+  	resp1.headers.set('Set-Cookie',`cookie-url=>${ab_split_url}`);
+  	variant = data.variants.indexOf(ab_split_url);
+  }
 
-  	return  new HTMLRewriter().on('a', new AttributeRewriter('href'))
-  							  .on('title',new AttributeRewriter(null,'title',variant))
-  							  .on('h1',new AttributeRewriter(null,'h1',variant))
-  							  .on('p',new AttributeRewriter(null,'p',variant)).transform(resp1)
+  	 	
+  // Changes in the HTML content of the variants @akd
+  return  new HTMLRewriter().on('a', new AttributeRewriter('href'))
+  							            .on('title',new AttributeRewriter(null,'title',variant))
+  							            .on('h1',new AttributeRewriter(null,'h1',variant))
+  							            .on('p',new AttributeRewriter(null,'p',variant))
+                            .transform(resp1)
 
 }
 
+// Class to perform Changes in the HTML content of the variants @akd
 class AttributeRewriter {
   constructor(attributeName,tag,variant) {
     this.attributeName = attributeName
@@ -59,6 +88,7 @@ class AttributeRewriter {
     this.variant = variant
   }
 
+  // For the tag attributes
   element(element) {
 
     const attribute = element.getAttribute(this.attributeName)
@@ -70,6 +100,7 @@ class AttributeRewriter {
     }
   }
 
+  //For text of the tags
   text(text){
   	
   	if (this.tag == 'title' && this.variant  == 0){  		
